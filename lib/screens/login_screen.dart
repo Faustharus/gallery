f40ec4bd29e521346c8e54aec5c4049d101ab4ed
Dams920 <50359853+Dams920@users.noreply.github.com>
2022-02-15 09:58:28 +0100

    Fixing for the login & signup screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -21,158 +21,158 @@ class _LoginScreenState extends State<LoginScreen> {
 
   final _auth = FirebaseAuth.instance;
 
-  // Email Placeholder
-  final titleEmail = const Text(
-    "Adresse Email",
-    style: TextStyle(
-      fontFamily: 'Poppins',
-      fontSize: 12,
-      color: Color(0xff02132b),
-      fontWeight: FontWeight.w500,
-      height: 1.6666666666666667,
-    ),
-  );
-
-  // Email Field
-  final emailField = TextFormField(
-    autofocus: false,
-    controller: emailController,
-    keyboardType: TextInputType.emailAddress,
-    validator: (value) {
-      if (value!.isEmpty) {
-        return ("Please enter your Email");
-      }
-      if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]").hasMatch(value)) {
-        return ("Please enter a Valid Email");
-      }
-      return null;
-    },
-    onSaved: (value) {
-      emailController.text = value!;
-    },
-    textInputAction: TextInputAction.next,
-    style: const TextStyle(
-      fontFamily: 'Poppins',
-      fontSize: 13,
-      color: Color(0x6902132b),
-      fontWeight: FontWeight.w500,
-      height: 1.7692307692307692,
-    ),
-    decoration: InputDecoration(
-      prefixIcon: const Icon(Icons.mail),
-      contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-      hintText: "john.doe@gmail.com",
-      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-    ),
-  );
-
-  // Password Placeholder
-  final titlePassword = const Text(
-    "Mot de Passe",
-    style: TextStyle(
-      fontFamily: 'Poppins',
-      fontSize: 12,
-      color: Color(0xff02132b),
-      fontWeight: FontWeight.w500,
-      height: 1.6666666666666667,
-    ),
-  );
-
-  // Password Field
-  final passwordField = TextFormField(
-    autofocus: false,
-    obscureText: true,
-    controller: passwordController,
-    validator: (value) {
-      RegExp regex = new RegExp(r'^.{6,}$');
-      if (value!.isEmpty) {
-        return ("Password is required for login");
-      }
-      if (!regex.hasMatch(value)) {
-        return ("Enter valid password (Min. of 6 characters)");
-      }
-    },
-    onSaved: (value) {
-      passwordController.text = value!;
-    },
-    textInputAction: TextInputAction.done,
-    style: const TextStyle(
-      fontFamily: 'Poppins',
-      fontSize: 13,
-      color: Color(0x6902132b),
-      fontWeight: FontWeight.w500,
-      height: 1.7692307692307692,
-    ),
-    decoration: InputDecoration(
-      prefixIcon: const Icon(Icons.vpn_key),
-      contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-      hintText: "Mot de Passe",
-      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-    ),
-  );
-
-  // Reset Password Button
-  final resetPassword = const Text.rich(
-    TextSpan(
-      style: TextStyle(
-        fontFamily: 'Poppins',
-        fontSize: 13,
-        color: Color(0xffa7adb5),
-        height: 2.4615384615384617,
-      ),
-      children: [
-        TextSpan(
-          text: 'Mot de passe oublié ? ',
-          style: TextStyle(
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        TextSpan(
-          text: 'Réinitialiser',
-          style: TextStyle(
-            color: Color(0xff02132b),
-            fontWeight: FontWeight.w500,
-            decoration: TextDecoration.underline,
-          ),
-        ),
-      ],
-    ),
-  );
-
-  // Login Button
-  final loginButton = Material(
-    elevation: 5,
-    borderRadius: BorderRadius.circular(30),
-    color: Colors.black,
-    child: const SizedBox(
-      width: 316.0,
-      height: 50,
-      child: MaterialButton(
-        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-        minWidth: MediaQuery.of(context).size.width,
-        onPressed: () {
-          signIn(emailController.text, passwordController.text);
-        },
-        child: Text(
-          'Connexion',
-          style: TextStyle(
-            fontFamily: 'Poppins',
-            fontSize: 14,
-            color: Color(0xffffffff),
-            fontWeight: FontWeight.w400,
-            height: 1.2142857142857142,
-          ),
-          textHeightBehavior:
-              TextHeightBehavior(applyHeightToFirstAscent: false),
-          textAlign: TextAlign.center,
-          softWrap: false,
-        ),
-      ),
-    ),
-  );
-
   @override
   Widget build(BuildContext context) {
+    // Email Placeholder
+    final titleEmail = const Text(
+      "Adresse Email",
+      style: TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 12,
+        color: Color(0xff02132b),
+        fontWeight: FontWeight.w500,
+        height: 1.6666666666666667,
+      ),
+    );
+
+    // Email Field
+    final emailField = TextFormField(
+      autofocus: false,
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Please enter your Email");
+        }
+        if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]").hasMatch(value)) {
+          return ("Please enter a Valid Email");
+        }
+        return null;
+      },
+      onSaved: (value) {
+        emailController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      style: const TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 13,
+        color: Color(0x6902132b),
+        fontWeight: FontWeight.w500,
+        height: 1.7692307692307692,
+      ),
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.mail),
+        contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "john.doe@gmail.com",
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+      ),
+    );
+
+    // Password Placeholder
+    final titlePassword = const Text(
+      "Mot de Passe",
+      style: TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 12,
+        color: Color(0xff02132b),
+        fontWeight: FontWeight.w500,
+        height: 1.6666666666666667,
+      ),
+    );
+
+    // Password Field
+    final passwordField = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      controller: passwordController,
+      validator: (value) {
+        RegExp regex = new RegExp(r'^.{6,}$');
+        if (value!.isEmpty) {
+          return ("Password is required for login");
+        }
+        if (!regex.hasMatch(value)) {
+          return ("Enter valid password (Min. of 6 characters)");
+        }
+      },
+      onSaved: (value) {
+        passwordController.text = value!;
+      },
+      textInputAction: TextInputAction.done,
+      style: const TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 13,
+        color: Color(0x6902132b),
+        fontWeight: FontWeight.w500,
+        height: 1.7692307692307692,
+      ),
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.vpn_key),
+        contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Mot de Passe",
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+      ),
+    );
+
+    // Reset Password Button
+    final resetPassword = const Text.rich(
+      TextSpan(
+        style: TextStyle(
+          fontFamily: 'Poppins',
+          fontSize: 13,
+          color: Color(0xffa7adb5),
+          height: 2.4615384615384617,
+        ),
+        children: [
+          TextSpan(
+            text: 'Mot de passe oublié ? ',
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          TextSpan(
+            text: 'Réinitialiser',
+            style: TextStyle(
+              color: Color(0xff02132b),
+              fontWeight: FontWeight.w500,
+              decoration: TextDecoration.underline,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    // Login Button
+    final loginButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.black,
+      child: SizedBox(
+        width: 316.0,
+        height: 50,
+        child: MaterialButton(
+          padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+          minWidth: MediaQuery.of(context).size.width,
+          onPressed: () {
+            signIn(emailController.text, passwordController.text);
+          },
+          child: const Text(
+            'Connexion',
+            style: TextStyle(
+              fontFamily: 'Poppins',
+              fontSize: 14,
+              color: Color(0xffffffff),
+              fontWeight: FontWeight.w400,
+              height: 1.2142857142857142,
+            ),
+            textHeightBehavior:
+                TextHeightBehavior(applyHeightToFirstAscent: false),
+            textAlign: TextAlign.center,
+            softWrap: false,
+          ),
+        ),
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -25,216 +25,216 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   final passwordEditingController = new TextEditingController();
   final confirmPasswordEditingController = new TextEditingController();
 
-  // FullName Placeholder
-  final titleFullName = const Text(
-    "Nom Complet",
-    style: TextStyle(
-      fontFamily: 'Poppins',
-      fontSize: 12,
-      color: Color(0xff02132b),
-      fontWeight: FontWeight.w500,
-      height: 1.6666666666666667,
-    ),
-  );
-
-  // FullName Field
-  final fullNameField = TextFormField(
-    autofocus: false,
-    controller: fullNameEditingController,
-    validator: (value) {
-      RegExp regex = new RegExp(r'^.{6,}$');
-      if (value!.isEmpty) {
-        return ("Full Name is required for login");
-      }
-      if (!regex.hasMatch(value)) {
-        return ("Enter a Name of Min. 6 characters");
-      }
-    },
-    onSaved: (value) {
-      fullNameEditingController.text = value!;
-    },
-    textInputAction: TextInputAction.done,
-    style: const TextStyle(
-      fontFamily: 'Poppins',
-      fontSize: 13,
-      color: Color(0x6902132b),
-      fontWeight: FontWeight.w500,
-      height: 1.7692307692307692,
-    ),
-    decoration: InputDecoration(
-      prefixIcon: const Icon(Icons.account_circle),
-      contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-      hintText: "Nom Complet",
-      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-    ),
-  );
-
-  // Email Placeholder
-  final titleEmail = const Text(
-    "Adresse Email",
-    style: TextStyle(
-      fontFamily: 'Poppins',
-      fontSize: 12,
-      color: Color(0xff02132b),
-      fontWeight: FontWeight.w500,
-      height: 1.6666666666666667,
-    ),
-  );
-
-  // Email Field
-  final emailField = TextFormField(
-    autofocus: false,
-    controller: emailEditingController,
-    keyboardType: TextInputType.emailAddress,
-    validator: (value) {
-      if (value!.isEmpty) {
-        return ("Please enter your Email");
-      }
-      if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]").hasMatch(value)) {
-        return ("Please enter a Valid Email");
-      }
-      return null;
-    },
-    onSaved: (value) {
-      emailEditingController.text = value!;
-    },
-    textInputAction: TextInputAction.next,
-    style: const TextStyle(
-      fontFamily: 'Poppins',
-      fontSize: 13,
-      color: Color(0x6902132b),
-      fontWeight: FontWeight.w500,
-      height: 1.7692307692307692,
-    ),
-    decoration: InputDecoration(
-      prefixIcon: const Icon(Icons.mail),
-      contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-      hintText: "john.doe@gmail.com",
-      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-    ),
-  );
-
-  // Password Placeholder
-  final titlePassword = const Text(
-    "Mot de Passe",
-    style: TextStyle(
-      fontFamily: 'Poppins',
-      fontSize: 12,
-      color: Color(0xff02132b),
-      fontWeight: FontWeight.w500,
-      height: 1.6666666666666667,
-    ),
-  );
-
-  // Password Field
-  final passwordField = TextFormField(
-    autofocus: false,
-    obscureText: true,
-    controller: passwordEditingController,
-    validator: (value) {
-      RegExp regex = new RegExp(r'^.{6,}$');
-      if (value!.isEmpty) {
-        return ("Password is required for login");
-      }
-      if (!regex.hasMatch(value)) {
-        return ("Enter valid password (Min. of 6 characters)");
-      }
-    },
-    onSaved: (value) {
-      passwordEditingController.text = value!;
-    },
-    textInputAction: TextInputAction.done,
-    style: const TextStyle(
-      fontFamily: 'Poppins',
-      fontSize: 13,
-      color: Color(0x6902132b),
-      fontWeight: FontWeight.w500,
-      height: 1.7692307692307692,
-    ),
-    decoration: InputDecoration(
-      prefixIcon: const Icon(Icons.vpn_key),
-      contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-      hintText: "Mot de Passe",
-      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-    ),
-  );
-
-  // Password Confirmation Placeholder
-  final titleConfirmPassword = const Text(
-    "Confirmation Mot de Passe",
-    style: TextStyle(
-      fontFamily: 'Poppins',
-      fontSize: 12,
-      color: Color(0xff02132b),
-      fontWeight: FontWeight.w500,
-      height: 1.6666666666666667,
-    ),
-  );
-
-  // Password Field
-  final confirmPasswordField = TextFormField(
-    autofocus: false,
-    obscureText: true,
-    controller: confirmPasswordEditingController,
-    validator: (value) {
-      if (confirmPasswordEditingController.text !=
-          passwordEditingController.text) {
-        return "Password do not match";
-      }
-      return null;
-    },
-    onSaved: (value) {
-      confirmPasswordEditingController.text = value!;
-    },
-    textInputAction: TextInputAction.done,
-    style: const TextStyle(
-      fontFamily: 'Poppins',
-      fontSize: 13,
-      color: Color(0x6902132b),
-      fontWeight: FontWeight.w500,
-      height: 1.7692307692307692,
-    ),
-    decoration: InputDecoration(
-      prefixIcon: const Icon(Icons.vpn_key),
-      contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-      hintText: "Mot de Passe",
-      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-    ),
-  );
-
-  // SignUp Button
-  final signUpButton = Material(
-    elevation: 5,
-    borderRadius: BorderRadius.circular(30),
-    color: Colors.black,
-    child: const SizedBox(
-      width: 316.0,
-      height: 50,
-      child: MaterialButton(
-        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-        onPressed: () {
-          signUp(emailEditingController.text, passwordEditingController.text);
-        },
-        child: Text(
-          'Inscription',
-          style: TextStyle(
-            fontFamily: 'Poppins',
-            fontSize: 14,
-            color: Color(0xffffffff),
-            fontWeight: FontWeight.w400,
-            height: 1.2142857142857142,
-          ),
-          textHeightBehavior:
-              TextHeightBehavior(applyHeightToFirstAscent: false),
-          textAlign: TextAlign.center,
-          softWrap: false,
-        ),
-      ),
-    ),
-  );
-
   @override
   Widget build(BuildContext context) {
+    // FullName Placeholder
+    final titleFullName = const Text(
+      "Nom Complet",
+      style: TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 12,
+        color: Color(0xff02132b),
+        fontWeight: FontWeight.w500,
+        height: 1.6666666666666667,
+      ),
+    );
+
+    // FullName Field
+    final fullNameField = TextFormField(
+      autofocus: false,
+      controller: fullNameEditingController,
+      validator: (value) {
+        RegExp regex = new RegExp(r'^.{6,}$');
+        if (value!.isEmpty) {
+          return ("Full Name is required for login");
+        }
+        if (!regex.hasMatch(value)) {
+          return ("Enter a Name of Min. 6 characters");
+        }
+      },
+      onSaved: (value) {
+        fullNameEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.done,
+      style: const TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 13,
+        color: Color(0x6902132b),
+        fontWeight: FontWeight.w500,
+        height: 1.7692307692307692,
+      ),
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.account_circle),
+        contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Nom Complet",
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+      ),
+    );
+
+    // Email Placeholder
+    final titleEmail = const Text(
+      "Adresse Email",
+      style: TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 12,
+        color: Color(0xff02132b),
+        fontWeight: FontWeight.w500,
+        height: 1.6666666666666667,
+      ),
+    );
+
+    // Email Field
+    final emailField = TextFormField(
+      autofocus: false,
+      controller: emailEditingController,
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Please enter your Email");
+        }
+        if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]").hasMatch(value)) {
+          return ("Please enter a Valid Email");
+        }
+        return null;
+      },
+      onSaved: (value) {
+        emailEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      style: const TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 13,
+        color: Color(0x6902132b),
+        fontWeight: FontWeight.w500,
+        height: 1.7692307692307692,
+      ),
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.mail),
+        contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "john.doe@gmail.com",
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+      ),
+    );
+
+    // Password Placeholder
+    final titlePassword = const Text(
+      "Mot de Passe",
+      style: TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 12,
+        color: Color(0xff02132b),
+        fontWeight: FontWeight.w500,
+        height: 1.6666666666666667,
+      ),
+    );
+
+    // Password Field
+    final passwordField = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      controller: passwordEditingController,
+      validator: (value) {
+        RegExp regex = new RegExp(r'^.{6,}$');
+        if (value!.isEmpty) {
+          return ("Password is required for login");
+        }
+        if (!regex.hasMatch(value)) {
+          return ("Enter valid password (Min. of 6 characters)");
+        }
+      },
+      onSaved: (value) {
+        passwordEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.done,
+      style: const TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 13,
+        color: Color(0x6902132b),
+        fontWeight: FontWeight.w500,
+        height: 1.7692307692307692,
+      ),
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.vpn_key),
+        contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Mot de Passe",
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+      ),
+    );
+
+    // Password Confirmation Placeholder
+    final titleConfirmPassword = Text(
+      "Confirmation Mot de Passe",
+      style: TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 12,
+        color: Color(0xff02132b),
+        fontWeight: FontWeight.w500,
+        height: 1.6666666666666667,
+      ),
+    );
+
+    // Password Field
+    final confirmPasswordField = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      controller: confirmPasswordEditingController,
+      validator: (value) {
+        if (confirmPasswordEditingController.text !=
+            passwordEditingController.text) {
+          return "Password do not match";
+        }
+        return null;
+      },
+      onSaved: (value) {
+        confirmPasswordEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.done,
+      style: const TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 13,
+        color: Color(0x6902132b),
+        fontWeight: FontWeight.w500,
+        height: 1.7692307692307692,
+      ),
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.vpn_key),
+        contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Mot de Passe",
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+      ),
+    );
+
+    // SignUp Button
+    final signUpButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.black,
+      child: SizedBox(
+        width: 316.0,
+        height: 50,
+        child: MaterialButton(
+          padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+          onPressed: () {
+            signUp(emailEditingController.text, passwordEditingController.text);
+          },
+          child: const Text(
+            'Inscription',
+            style: TextStyle(
+              fontFamily: 'Poppins',
+              fontSize: 14,
+              color: Color(0xffffffff),
+              fontWeight: FontWeight.w400,
+              height: 1.2142857142857142,
+            ),
+            textHeightBehavior:
+                TextHeightBehavior(applyHeightToFirstAscent: false),
+            textAlign: TextAlign.center,
+            softWrap: false,
+          ),
+        ),
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
